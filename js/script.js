3e0b91d357763a2016db94221675646af3725aab
Luiz Lopes <luizlopes12@outlook.com>
2021-05-13 13:35:15 -0300

    pass mostrar

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function showPass(){
+    const eye = document.querySelector('#eye');
+    const eyeSlash = document.querySelector('#eye-slash');
+    const fieldPass = document.querySelector('#field-password');
+   
+   if(eye.style.display === 'none'){
+       eye.style.display = 'block';
+       eyeSlash.style.display = 'none';
+       fieldPass.type = 'text'
+   }
+   else{
+       eye.style.display = 'none';
+       eyeSlash.style.display = 'block';
+       fieldPass.type = 'password'
+   }
+   }
