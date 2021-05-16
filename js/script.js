ba78435a6cd0671ea195151cd35079ce0b5fdfb6
Luiz Lopes <luizlopes12@outlook.com>
2021-05-16 17:20:59 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
+/*Função de mostrar/esconder senha*/
 function showPass(){
     const eye = document.querySelector('#eye');
     const eyeSlash = document.querySelector('#eye-slash');
     const fieldPass = document.querySelector('#field-password');
    
+    //mudar para o tipo text 
    if(eye.style.display === 'none'){
        eye.style.display = 'block';
        eyeSlash.style.display = 'none';
        fieldPass.type = 'text'
    }
+   //mudar para o tipo password
    else{
        eye.style.display = 'none';
        eyeSlash.style.display = 'block';
